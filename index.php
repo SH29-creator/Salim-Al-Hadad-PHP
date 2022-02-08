947f5a3ed830e583196b49219f2fabdc689f01d1
Salim Al Hadad <90275039+SH29-creator@users.noreply.github.com>
2022-02-08 12:18:21 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+	<title>Belajar PHP</title>
+  </head>
+  <body>
+  
+  <h1>Sekolah Koding</h1>
+  
+    <?php
+	  
+	  echo "Halo";
+	  echo "Member";
+	?>
+	
+	Halo semuanya!
+	
+  </body>
+</html>
